8ad9c1673e9ba6a68e2f3a81d5ed6bd0c18811cb
Johannes Kolb <johannes.kolb@gmx.net>
2012-11-21 23:35:01 +0100

    improve test runner

--- test/t/geometry_geos/test_point_geometry.cpp
+++ test/t/geometry_geos/test_point_geometry.cpp
@@ -8,6 +8,7 @@
 #include <osmium/geometry/point.hpp>
 #include <osmium/geometry/geos.hpp>
 
+BOOST_AUTO_TEST_SUITE(GEOS)
 BOOST_AUTO_TEST_SUITE(PointGeometry)
 
 BOOST_AUTO_TEST_CASE(instantiation) {
@@ -29,4 +30,4 @@ BOOST_AUTO_TEST_CASE(geos_geometry) {
 }
 
 BOOST_AUTO_TEST_SUITE_END()
-
+BOOST_AUTO_TEST_SUITE_END()
